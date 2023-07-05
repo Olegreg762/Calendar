ac7486938824f31de5ba0d81063d6d5aeb7850de
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-07-05 14:14:03 -0500

    time slots will now highlight colors based on time

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,60 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+let date = new Date();
+let hours = date.getHours();
+// const hour_template = document.querySelector('#hour-9');
+// const number_hours = 18;
+// let pm_am = "";
+// for(let i = 10; i < number_hours; i++){
+//   const clone = hour_template.parentElement.cloneNode(true);
+//   clone.id = `hour-${i}`;
+//   const hour_text = clone.querySelector('.hour');
+//   let hour = i;
+//   if(i<12){
+//     pm_am = "AM"
+//   }else{
+//     pm_am = "PM"
+//     hour -=12;
+//   }
+//   if(hour == 0){
+//     hour = 12
+//   }
+//   hour_text.textContent = `${hour+pm_am}`;
+//   document.body.appendChild(clone);
 
-const hour_template = document.querySelector('#hour-9');
-const number_hours = 18;
-let pm_am = "";
-for(let i = 10; i < number_hours; i++){
-  const clone = hour_template.parentElement.cloneNode(true);
-  clone.id = `hour-${i}`;
-  const hour_text = clone.querySelector('.hour');
-  let hour = i;
-  if(i<12){
+// }
+function date_time(){
+  hours = date.getHours();
+  minutes = date.getMinutes();
+  if(minutes<10){
+    minutes="0"+date.getMinutes()
+  }
+  if(hours<12){
     pm_am = "AM"
   }else{
     pm_am = "PM"
-    hour -=12;
+    hours -= 12 
   }
-  if(hour == 0){
-    hour = 12
-  }
-  hour_text.textContent = `${hour+pm_am}`;
-  document.body.appendChild(clone);
+  document.getElementById("currentDay").textContent = `Today's Date ${date.getMonth()}/${date.getDay()}/${date.getFullYear()} ${hours}:${minutes}${pm_am}`;
+} 
+date_time()
+setInterval(date_time,1000);
 
+
+hours = date.getHours();
+let ids = 18;
+for(let i = 9; i<ids;i++ ){
+let element_hour = document.getElementById(`hour-${i}`);
+let element_hour_id = element_hour.id;
+let element_hour_id_value = element_hour_id.replace('hour-', '');
+if(element_hour_id_value == hours){
+  element_hour.classList.remove('past');
+  element_hour.classList.add('present');
+}else if(element_hour_id_value > hours){
+  element_hour.classList.remove('past');
+  element_hour.classList.add('future');
+}
 }
 
 
